36f7fac5d65dff7a5d8794c1c19338ffc6cc31c2
Vishwajit Kolathur <ajit@labyrinth.io>
2013-11-12 20:45:20 +0530

    Just Started, First Commit

---
+++ Assignment1.py
@@ -0,0 +1,7 @@
+__author__ = 'ajit'
+# Bayesian learning on the data;
+# TODO: Scan training data into data structure
+# TODO: Process the training data to obtain the intelligence, Bayesian Learning
+# TODO: Dump intelligence file for testing
+# Assignment 1: Vishwajit Kolathur Nov 12th 2013
+
